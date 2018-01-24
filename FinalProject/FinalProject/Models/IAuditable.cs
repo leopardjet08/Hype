f496cf8236df2e0c16ca1c48d1ade056c23950ae
Niagara College <helpdesk@niagaracollege.ca>
2018-01-24 14:56:39 -0500

    IAuditable

--- FinalProject/FinalProject/Models/IAuditable.cs
+++ FinalProject/FinalProject/Models/IAuditable.cs
@@ -5,7 +5,12 @@ using System.Web;
 
 namespace FinalProject.Models
 {
-    public class IAuditable
+    internal interface IAuditable
     {
+        string CreatedBy { get; set; }
+        DateTime? CreatedOn { get; set; }
+        string UpdatedBy { get; set; }
+        DateTime? UpdatedOn { get; set; }
+
     }
 }
