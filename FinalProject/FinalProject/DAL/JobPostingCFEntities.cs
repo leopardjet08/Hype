273233439368028b7e5825b0f3596ca717df5283
Raldjeet Maslog <rmaslog1@ncstudents.niagaracollege.ca>
2018-02-19 13:57:53 -0500

    Seeding is done

--- FinalProject/FinalProject/DAL/JobPostingCFEntities.cs
+++ FinalProject/FinalProject/DAL/JobPostingCFEntities.cs
@@ -11,21 +11,20 @@ namespace FinalProject.DAL
 {
     public class JobPostingCFEntities : DbContext
     {
-        public DbSet<Applicant> Applicants { get; set; }
-        public DbSet<Application> Applications { get; set; }
-        public DbSet<ApplicationStatus> ApplicationStatus { get; set; }
-      
-        public DbSet<BestCandidate> BestCandidates { get; set; }
-        public DbSet<City> Cities { get; set; }
         public DbSet<Job> Jobs { get; set; }
-        public DbSet<Posting> Postings { get; set; }
         public DbSet<Province> Provinces { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<ApplicationStatus> ApplicationStatus { get; set; }
+        public DbSet<SchoolLevel> SchoolLevels { get; set; }
+        public DbSet<SchoolFamily> SchoolFamilies { get; set; }
         public DbSet<Qualification> Qualifications { get; set; }
         public DbSet<Requirement> Requirements { get; set; }
-        public DbSet<SavedPosting> SavedPostings { get; set; }
         public DbSet<School> Schools { get; set; }
-        public DbSet<SchoolFamily> SchoolFamilies { get; set; }
-        public DbSet<SchoolLevel> SchoolLevels { get; set; }
+        public DbSet<Applicant> Applicants { get; set; }
+        public DbSet<Posting> Postings { get; set; }
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<BestCandidate> BestCandidates { get; set; }
+        public DbSet<SavedPosting> SavedPostings { get; set; }
         public DbSet<UserPhoto> UserPhotos { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
