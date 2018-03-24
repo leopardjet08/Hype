c3428e3ea2e0ca7709fe2b7bff97c4510ba02b84
Raldjeet Maslog <rmaslog1@ncstudents.niagaracollege.ca>
2018-03-24 03:55:41 -0400

    for now

--- FinalProject/FinalProject/Controllers/HomeController.cs
+++ FinalProject/FinalProject/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace FinalProject.Controllers
             
             ViewBag.Filtering = "";
 
-            var postings = from s in db.Postings select s;
+            var postings = from s in db.Postings where ((DateTime)s.ClosingDate >= DateTime.Today) select s;
 
             ViewBag.ShowList = false;
 
