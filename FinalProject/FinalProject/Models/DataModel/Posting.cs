881c965fac3d96d21586de6cd4c529bbebaea72b
Raldjeet Maslog <rmaslog1@ncstudents.niagaracollege.ca>
2018-03-30 13:08:11 -0400

    w

--- FinalProject/FinalProject/Models/DataModel/Posting.cs
+++ FinalProject/FinalProject/Models/DataModel/Posting.cs
@@ -31,6 +31,13 @@ namespace FinalProject.Models
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? StartDate { get; set; }
 
+
+        [Display(Name = "EndDate Job")]
+        [Required(ErrorMessage = "You must specify the End date of the job.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime JobEndDate { get; set; }
+
         [Display(Name = "Posting Description")]
         [StringLength(2000, ErrorMessage = "Description must be between 20 and 2000 characters")]
         [DataType(DataType.MultilineText)]
@@ -39,19 +46,24 @@ namespace FinalProject.Models
         [Display(Name = "FTE Status")]
         [Required(ErrorMessage = "You must specify employment status.")]
         [Range(0, 1.5, ErrorMessage = "FTE number cannot exceed 1.5")]
-        public double fte  { get; set;}
-        
+        public double Fte { get; set; }
+
         [Required(ErrorMessage = "Please specify the school you applied for.")]
         public int SchoolID { get; set; }
 
-        [Required(ErrorMessage ="Please provide the Job for this posting")]
+        [Required(ErrorMessage = "Please provide the Job for this posting")]
         public int JobID { get; set; }
 
         public virtual School School { get; set; }
         public virtual Job Job { get; set; }
         public ICollection<Application> Applications { get; set; }
         public ICollection<SavedPosting> SavedPostings { get; set; }
-        
+
+        //many to many of posting to skill,requirment,qualification
+        public virtual ICollection<Skill> Skills { get; set; }
+        public virtual ICollection<Requirement> Requirements { get; set; }
+        public virtual ICollection<Qualification> Qualifications { get; set; }
+
         // Validation for date
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
@@ -63,6 +75,12 @@ namespace FinalProject.Models
             {
                 yield return new ValidationResult("The start date for the posting cannot be before the closing date.", new[] { "StartDate" });
             }
+
+            if (JobEndDate < StartDate)
+            {
+                yield return new ValidationResult("The Job End date cannot be before the job Start date.", new[] { "JobEndDate" });
+            }
+
         }
 
     }
