f356c3b8aa12f2f7b67a14c3a9b8e66d0494d9d7
Raldjeet Maslog <rmaslog1@ncstudents.niagaracollege.ca>
2018-03-30 14:58:26 -0400

    InitalCreate

--- FinalProject/FinalProject/DAL/Migrations/201803301857299_initialJet.Designer.cs
+++ FinalProject/FinalProject/DAL/Migrations/201803301857299_initialJet.Designer.cs
@@ -7,13 +7,13 @@ namespace FinalProject.DAL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class g : IMigrationMetadata
+    public sealed partial class initialJet : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(g));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initialJet));
         
         string IMigrationMetadata.Id
         {
-            get { return "201803301725267_g"; }
+            get { return "201803301857299_initialJet"; }
         }
         
         string IMigrationMetadata.Source
--- FinalProject/FinalProject/DAL/Migrations/201803301857299_initialJet.cs
+++ FinalProject/FinalProject/DAL/Migrations/201803301857299_initialJet.cs
@@ -3,7 +3,7 @@ namespace FinalProject.DAL.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class g : DbMigration
+    public partial class initialJet : DbMigration
     {
         public override void Up()
         {
